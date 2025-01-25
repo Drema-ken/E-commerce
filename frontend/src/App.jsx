869e3529fc8c02abaf89bdf819fbc2fcd2bcb911
drema ken <dremajnr@gmail.com>
2025-01-25 14:23:14 +0100

    Finished Footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
